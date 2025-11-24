e1d15e58c9da9102a09a789ab6d108d64c2938a4
Dan Morris <agentmorris@gmail.com>
2025-11-23 16:31:52 -0800

    geofence validation in build_geofence_release

--- speciesnet/scripts/build_geofence_release.py
+++ speciesnet/scripts/build_geofence_release.py
@@ -12,17 +12,73 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Script to build the geofence release from geofence base with extra manual fixes."""
+"""Script to build the geofence release from geofence base with extra manual fixes.
 
+A geofencing .json file defines a "global geofencing dict".
+
+Keys in a global geofencing dict are five-token SpeciesNet taxonomy strings,
+for example:
+
+aves;accipitriformes;accipitridae;accipiter;rhodogaster
+
+Each item in a global geofencing dict is a "taxon geofencing dict", containing
+"allow" and/or "block" rules for that taxon.  The only valid keys in a taxon
+geofencing dict are "allow" and "block".  In the base geofence file, only "allow"
+rules are valid.
+
+"allow" or block" are mapped a to "regional rules dict".  Each key in a regional
+rules dict is a three-letter country code, and the correponding value is a list
+of regional rules within that country.  Currently that list is empty for all country
+codes other than "USA".
+
+Examples:
+
+This taxon would be allowed in RUS, SGP, THA, TWAN, and VNM.  In the USA,
+it would be allowed in AK but blocked in all other states.  It would be
+blocked in all other countries.
+
+    "aves;accipitriformes;accipitridae;accipiter;soloensis": {
+        "allow": {
+            "RUS": [],
+            "SGP": [],
+            "THA": [],
+            "TWN": [],
+            "USA": [
+                "AK"
+            ],
+            "VNM": []
+        }
+    }
+
+This taxon would be blocked in ABW and AFG, allowed everywhere else:
+
+    "mammalia;cetartiodactyla;hippopotamidae;;": {
+        "block": {
+            "ABW": [],
+            "AFG": []
+        }
+
+Additional conventions:
+
+* If a taxon is not included in the geofence, it's allowed everywhere.
+* If allow rules exist for a taxon, any country not on the allow-list for that
+  taxon is blocked.
+* Block rules "win" over allow rules.  Taxa that are allowed in the base geofence
+  may later get blocked
+
+"""
 import copy
 import json
 from pathlib import Path
 from typing import Union
+from typing import Optional
 
 from absl import app
 from absl import flags
 from absl import logging
 import pandas as pd
+
+from speciesnet.geofence_utils import should_geofence_animal_classification
 
 _BASE = flags.DEFINE_string(
     "base",
@@ -52,7 +108,165 @@ _OUTPUT = flags.DEFINE_string(
 StrPath = Union[str, Path]
 
 
+def _taxon_allowed_in_region(
+    label: str, country: str, admin1_region: Optional[str], geofence_map: dict
+) -> bool:
+    """Utility function to check whether a taxon is allowed in a
+    region.  This is a thin wrapper for should_geofence_animal_classification.
+    """
+
+    # should_geofence_animal_classification accepts only seven-token
+    # taxon strings, we want to use five-token strings.
+    if len(label.split(";")) not in (5, 7):
+        raise ValueError(f"Illegal label {label}")
+
+    if len(label.split(";")) != 7:
+        label = ";" + label + ";"
+
+    return not should_geofence_animal_classification(
+        label, country, admin1_region, geofence_map, enable_geofence=True
+    )
+
+
+def _validate_taxon_string(taxon: str) -> bool:
+    """Validates a five-token taxon string.  Errors in invalid
+    taxa, else returns True."""
+
+    tokens = taxon.split(";")
+
+    if (not isinstance(taxon, str)) or (len(tokens) != 5):
+        print(f"Invalid taxon string {taxon} in geofence")
+        return False
+
+    # You can't specify, e.g., a species without a genus
+    found_non_empty_level = False
+    for token in tokens[::-1]:
+        if len(token) > 0:
+            found_non_empty_level = True
+        else:
+            if found_non_empty_level:
+                raise ValueError(f"Illegal taxon {taxon}")
+
+    return True
+
+
+def _generate_parent_taxon_strings(s):
+    """Given a five-token taxon string in a;b;c;d;e format,
+    generates the taxon strings for all parents (e.g. a;b;c;d;;).
+    """
+
+    tokens = s.split(";")
+    n_tokens = len(tokens)
+    assert n_tokens == 5
+    output_strings = []
+    i_token = n_tokens - 1
+    while i_token > 0:
+        # Skip tokens that are already empty
+        if len(tokens[i_token]) == 0:
+            i_token -= 1
+            continue
+        else:
+            tokens[i_token] = ""
+            output_string = ";".join(tokens)
+            output_strings.append(output_string)
+    return output_strings
+
+
+def validate_geofence(geofence: dict[str, dict]) -> bool:
+    """Validates a global geofencing dict.  See module header for
+    format rules.
+
+    Args:
+        path:
+            Filename of the base geofence .json file.
+
+    Returns:
+        True if the geofencing dict is valid, else False.
+    """
+
+    if not isinstance(geofence, dict):
+        print("Invalid geofence type")
+        return False
+
+    # Basic format validation
+    for taxon in geofence.keys():
+
+        # All keys should be five-token taxon strings
+        _validate_taxon_string(taxon)
+
+        taxon_rules = geofence[taxon]
+
+        for rule_type in taxon_rules.keys():
+
+            if (not isinstance(rule_type, str)) or (
+                rule_type not in ("allow", "block")
+            ):
+                print(f"Invalid rule type {rule_type} for taxon {taxon}")
+                return False
+
+            countries = taxon_rules[rule_type]
+
+            for country_code in countries.keys():
+                if (not isinstance(country_code, str)) or (len(country_code) != 3):
+                    print(f"Invalid country code {country_code} for taxon {taxon}")
+                    return False
+                regions = countries[country_code]
+                if not isinstance(regions, list):
+                    print(f"Invalid rules for {country_code} for taxon {taxon}")
+                    return False
+                if not all([isinstance(x, str) for x in regions]):
+                    print(f"Invalid regions for {country_code} for taxon {taxon}")
+                    return False
+
+    # Make sure that if a taxon is explicitly allowed in a region, all of its parents
+    # are allowed.
+    for taxon in geofence.keys():
+
+        if "allow" not in geofence[taxon]:
+            continue
+
+        parent_taxa = _generate_parent_taxon_strings(taxon)
+
+        allowed_countries = geofence[taxon]["allow"]
+        for country in allowed_countries.keys():
+            allowed_regions = allowed_countries[country]
+            if len(allowed_regions) == 0:
+                allowed_regions = [None]
+            for region in allowed_regions:
+                # We know this taxon is allowed in this region
+                assert _taxon_allowed_in_region(
+                    label=taxon,
+                    country=country,
+                    admin1_region=region,
+                    geofence_map=geofence,
+                )
+                for parent_taxon in parent_taxa:
+                    allowed = _taxon_allowed_in_region(
+                        label=parent_taxon,
+                        country=country,
+                        admin1_region=region,
+                        geofence_map=geofence,
+                    )
+                    if not allowed:
+                        raise ValueError(
+                            f"Parent taxon {parent_taxon} of {taxon} not allowed in "
+                            f"{country}:{region}"
+                        )
+
+    return True
+
+
 def load_geofence_base(path: StrPath) -> dict[str, dict]:
+    """Loads the geofence .json file.
+
+    Args:
+        path:
+            Filename of the base geofence .json file.
+
+    Returns:
+        A global geofencing dict.  See module header for format
+        information.
+    """
 
     with open(path, mode="r", encoding="utf-8") as fp:
         data = json.load(fp)
@@ -62,25 +276,37 @@ def load_geofence_base(path: StrPath) -> dict[str, dict]:
                 "Base geofence should provide only species-level rules. "
                 f"Found higher taxa rule with the label: `{label}`"
             )
-        if "block" in rules:
-            raise ValueError("Block rules are not accepted in base geofence.")
+        if (len(rules) != 1) or (next(iter(rules)) != "allow"):
+            raise ValueError("Only 'allow' rules are accepted in base geofence.")
     return data
 
 
 def fix_geofence_base(
     geofence_base: dict[str, dict], fixes_path: StrPath
 ) -> dict[str, dict]:
+    """Applies the changes specified in a geofence fixes .csv file
+    to the base global geofencing dict, returning an updated global geofencing
+    dict.
+
+    Args:
+        geofence_base:
+            A global geofencing dict, probably loaded via load_geofence_base
+        fixes_path:
+            Filename of the .csv file defining modifications to the geofencing
+            dict.
+
+    Returns:
+        An updated global geofencing dict.
+    """
 
     geofence = copy.deepcopy(geofence_base)
 
-    fixes = pd.read_csv(fixes_path, keep_default_na=False, comment='#')
+    fixes = pd.read_csv(fixes_path, keep_default_na=False, comment="#")
     for idx, fix in fixes.iterrows():
         label = fix["species"].lower()
         label_parts = label.split(";")
         if len(label_parts) != 5:
-            raise ValueError(
-                "Fixes should always use five-token taxon strings"
-            )
+            raise ValueError("Fixes should always use five-token taxon strings")
         rule = fix["rule"].lower()
         if rule not in {"allow", "block"}:
             raise ValueError(
@@ -136,6 +362,19 @@ def fix_geofence_base(
 
 
 def propagate_to_higher_taxa(geofence: dict[str, dict]) -> dict[str, dict]:
+    """Propagates rules up the taxonomy tree.  For example, if species X is allowed
+    in country Y, all taxonomic parents of X also need to be allowed in Y.
+
+    Args:
+        geofence: Dict of predictions, keyed by filepaths.
+
+    Returns:
+        Returns "classifier", "detector" or "ensemble" when the corresponding component
+        was identified as the source of predictions. Returns "invalid" when predictions
+        contain both classifications and detections, but couldn't identify results from
+        the ensemble. Returns "unknown" when no prediction is recognizable (e.g. when
+        there are only failures).
+    """
 
     new_geofence = {}
 
@@ -192,6 +431,8 @@ def main(argv: list[str]) -> None:
     del argv  # Unused.
 
     geofence_base = load_geofence_base(_BASE.value)
+    validate_geofence(geofence_base)
+
     geofence_release = fix_geofence_base(geofence_base, _FIXES.value)
     geofence_release = propagate_to_higher_taxa(geofence_release)
     if _TRIM.value:
@@ -202,6 +443,8 @@ def main(argv: list[str]) -> None:
         geofence_release = trim_to_supported_labels(geofence_release, _TRIM.value)
     else:
         logging.info("No trimming was performed.")
+    validate_geofence(geofence_release)
+
     save_geofence(geofence_release, _OUTPUT.value)
 
 
